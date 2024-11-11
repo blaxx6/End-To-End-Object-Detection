244d3e5e25d27c15313a2ccb821a510c59ca30e0
blaxx6 <141863210+blaxx6@users.noreply.github.com>
2024-11-11 18:56:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version='0.0.0',
+    author='Adarsh',
+    author_email='adarsh2006yadav@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)

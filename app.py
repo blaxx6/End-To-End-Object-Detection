ff04831bcf8dc13cf95098bdc34e087477b1eeff
blaxx6 <141863210+blaxx6@users.noreply.github.com>
2024-11-17 16:38:15 +0530

    model trainer and data validation added

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys
-#logging.info("welcome to the project")
-try:
-    a=7/'9'
-except Exception as e:
-    raise SignException(e,sys) from e
-    
-
+from signLanguage.pipeline.training_pipeline import TrainPipeline
+obj=TrainPipeline()
+obj.run_pipeline()
